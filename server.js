e355fe3820717b949057d08ab97ba2a8f55cb60f
Ravi Kumar <rav7179@gmail.com>
2024-08-08 23:00:09 +0530

    api done

---
+++ server.js
@@ -0,0 +1,33 @@
+import express from "express";
+const app = express();
+import cors from "cors";
+import { dataPage1, dataPage2 } from "./data.js";
+
+const corsOptions = {
+  origin: "https://rk-telegram.netlify.app/",
+  optionsSuccessStatus: 200,
+};
+
+console.log(dataPage2);
+console.log(dataPage1);
+
+app.use(express.json());
+
+app.use(cors(corsOptions));
+
+app.get("/api/get_all_chats", (req, res) => {
+  const page = req.query.page || 1; // Get the page number from query params
+  if (page == 1) {
+    res.json(dataPage1);
+  } else if (page == 2) {
+    res.json(dataPage2);
+  } else {
+    res.status(404).json({ message: "Page not found" });
+  }
+});
+
+// Start Server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
